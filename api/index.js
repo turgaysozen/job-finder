91baf831828683a30f8e3d5bd9097d130adcc281
turgaysozen <turgaysozen@hotmail.com>
2020-01-30 21:00:01 +0300

    heroku

--- api/index.js
+++ api/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3001;
+// const port = 3001;
 
 var redis = require("redis");
- client = redis.createClient();
+let client;
+
+if (process.env.REDIS_URL) {
+    client = redis.createClient(process.env.REDIS_URL);
+}
+else {
+    client = redis.createClient();
+}
+
 
 const { promisify } = require('util');
 const getAsync = promisify(client.get).bind(client);
+
+
 
 app.get('/jobs', async (req, res) => {
 
@@ -14,6 +24,8 @@ app.get('/jobs', async (req, res) => {
 
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.send(mergedJobs);
+
+    client.setex(req, 60, mergedJobs);
 });
 
 app.get('/jobs/page=:id', async (req, res) => {
@@ -33,5 +45,7 @@ app.get('/jobs/page=:id', async (req, res) => {
 app.get('/jobs/apicall', async (req, res) => {
     res.send('<h1>Api Call Page</h1><p>You can call jobs like: <b>http://localhost:3001/jobs/page=1</b></p>');
 });
-
+const port = process.env.PORT || 5000;
+app.listen(port);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
