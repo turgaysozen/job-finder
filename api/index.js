0fe32415a1b8a80046d12d9bb90718683bbdb366
turgaysozen <turgaysozen@hotmail.com>
2020-02-01 23:08:25 +0300

    heroku without redis

--- api/index.js
+++ api/index.js
@@ -2,16 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-// var redis = require("redis").createClient();
-var redis = require("redis"), redis_client = redis.createClient("redis://127.0.0.1:22318"); 
+var redis = require("redis").createClient();
 
-if (process.env.REDISTOGO_URL) {
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
-} else {
-    var redis = require("redis").createClient();
-}
+// if (process.env.REDISTOGO_URL) {
+//     var rtg = require("url").parse(process.env.REDISTOGO_URL);
+//     var redis = require("redis").createClient(rtg.port, rtg.hostname);
+//     redis.auth(rtg.auth.split(":")[1]);
+// } else {
+//     var redis = require("redis").createClient();
+// }
 
 const { promisify } = require('util');
 const getAsync = promisify(redis.get).bind(redis);
@@ -23,10 +22,12 @@ app.get('/jobs', async (req, res) => {
 
     const mergedJobs = await getAsync('MergedAllUniqueJobs');
     const mergedAllJobs = await getAsync('MergedAllJobs');
+    const jsonData = JSON.parse(mergedJobs);
+
     // console.log(mergedAllJobs)
     totalJobsCount = (JSON.parse(mergedAllJobs)).length;
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.send(mergedJobs);
+    res.send(jsonData);
 });
 
 app.get('/jobs/page=:id', async (req, res) => {
@@ -42,8 +43,25 @@ app.get('/jobs/page=:id', async (req, res) => {
     res.send(fiftieth);
 });
 
-app.get('/jobs/apicall', async (req, res) => {
-    res.send(`<h1>Api Call Page</h1><p>Total Jobs: ${totalJobsCount}, page count: ${Math.ceil(totalJobsCount / 50)} You can call jobs like: <b>http://localhost:3001/jobs/page=1</b></p>`);
+app.get('/jobs/apicall', async (req, res) => { 
+    let onPage = "${onPage}";
+    res.send(`
+    <h1>Api Call Explanation</h1><p>Total Jobs: ${totalJobsCount}, page count: ${Math.ceil(totalJobsCount / 50)} You can fetch jobs by: <b>http://localhost:3001/jobs/page=1<br/><br/>Example: </b></p>
+    <xmp>   let allJobs = [], onPage = 0;
+    //fetch all pages from github
+    while (onPage <= ${Math.ceil(totalJobsCount / 50)}) {
+        try {
+            const res = await fetch('http://localhost:3001/jobs/page=${onPage}');
+            const jobs = await res.json();
+            allJobs.push(...jobs);
+            onPage++;
+        } catch {
+            onPage = 0;
+            allJobs = [];
+        }
+    }
+</xmp>
+    `);
 });
 
 if (process.env.NODE_ENV === 'production') {
