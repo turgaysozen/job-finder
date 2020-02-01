8f89d4d345c098dfb3d46778ca0afffec9eee3d2
turgaysozen <turgaysozen@hotmail.com>
2020-02-01 13:03:29 +0300

    heroku update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-// const port = process.env.PORT || 3001;
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // var redis = require("redis");
 // let client;
