3013e51c40161efa09b8a9b33fb4121086b3bc1e
turgaysozen <turgaysozen@hotmail.com>
2020-02-10 00:50:57 +0300

    heroku deployment

--- api/index.js
+++ api/index.js
@@ -5,19 +5,21 @@ const path = require('path');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
-    });
-    // app.use('/', express.static(path.join(__dirname, 'client/build')));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    // });
-}
-else {
-    require('dotenv').config({ path: '.env' });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+//     });
+// }
+// else {
+//     require('dotenv').config({ path: '.env' });
+// }
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+/*React root*/
+app.get("*", (req, res) => {
+res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 const Job = require('../model/job');
 const mongoose = require('mongoose');
