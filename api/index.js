517de455bde30c366337bc2da8b52b1ef689c79d
turgaysozen <turgaysozen@hotmail.com>
2020-02-10 00:05:28 +0300

    heroku deployment

--- api/index.js
+++ api/index.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static('client/build'));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
-    // });
-    app.use('/', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
     });
+    // app.use('/', express.static(path.join(__dirname, 'client/build')));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    // });
 }
 else {
     require('dotenv').config({ path: '.env' });
