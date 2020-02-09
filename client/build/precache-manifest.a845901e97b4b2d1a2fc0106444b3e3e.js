28a61774d337189caac2e7204277a2bcf914553b
turgaysozen <turgaysozen@hotmail.com>
2020-02-10 01:37:09 +0300

    heroku deployment

---
+++ client/build/precache-manifest.a845901e97b4b2d1a2fc0106444b3e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5efcbf9130805a0c23de9177dfd72a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3114a332524849da8c",
+    "url": "/static/css/main.e105eba1.chunk.css"
+  },
+  {
+    "revision": "5adc208b874206cd92d1",
+    "url": "/static/js/2.4a23dd4a.chunk.js"
+  },
+  {
+    "revision": "ae025b7d75866b9fb21ac2eab416080f",
+    "url": "/static/js/2.4a23dd4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed3114a332524849da8c",
+    "url": "/static/js/main.51daf522.chunk.js"
+  },
+  {
+    "revision": "5274233e5f14eabd9ee9",
+    "url": "/static/js/runtime-main.38a53adb.js"
+  }
+]);
