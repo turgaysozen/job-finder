5b45def9187ad35e66a208c79747605a696fed73
turgaysozen <turgaysozen@hotmail.com>
2020-02-09 23:59:41 +0300

    heroku deployment

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
     // app.get('*', (req, res) => {
     //     res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
     // });
-    app.use('/', express.static(path.join(__dirname, '/client/build')));
+    app.use('/', express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
